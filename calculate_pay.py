53f83a7630f9f183b51692c78ffa4c933f1ec62e
WEN_LONG HUANG <whuang148@my.bcit.ca>
2023-02-06 10:58:07 -0800

    Update calculate_pay.py

--- calculate_pay.py
+++ calculate_pay.py
@@ -1,15 +1,28 @@
 def calculate_pay(hours, wage):
-    if hours <= 0 or wage <= 0
+    total_wage = 0
+    double_wage = 2 * wage
+    if hours <= 0 or wage <= 0:
         return 0
-    else:
-        while
+
+    if hours > 40:
+        range_of_hours = range(41, hours+1)
+        for i in range_of_hours:
+            wages
+    if hours <= 40:
+        total_wage += hours * wage
+
+    while hours > 40:
+        total_wage += hours * double_wage
+    return total_wage
+
 
 
 
 
 def main():
+    print("calculate_pay(10,10): ", calculate_pay(10, 10))
     print()
-    print()
+    print("calculate_pay(50,10): ", calculate_pay(50, 10))
 
 
 if __name__ == "__main__":
