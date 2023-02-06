aaf56a5e5e7e6f26584eba11df2fff5e128c58e6
WEN_LONG HUANG <whuang148@my.bcit.ca>
2023-02-06 10:45:45 -0800

    Add calculate_pay.py

---
+++ calculate_pay.py
@@ -0,0 +1,16 @@
+def calculate_pay(hours, wage):
+    if hours <= 0 or wage <= 0
+        return 0
+    else:
+        while
+
+
+
+
+def main():
+    print()
+    print()
+
+
+if __name__ == "__main__":
+    main()
